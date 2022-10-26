d57ba6aeef731051e7c15bfb66bf8d8d9c889c5a
Ogaga <iyaraogaga69@gmail.com>
2022-10-26 08:31:49 +0100

    Add mock images to build mock html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+// const url = async () => {
+//     const res = await fetch('https://api.tvmaze.com/shows')
+//     const data = await res.json()
+//     console.log(data)
+// }
+// url()
