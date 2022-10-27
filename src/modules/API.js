5d6fd4b484ed5b5a8307a69f1272a39e9b6d739b
Ogaga <iyaraogaga69@gmail.com>
2022-10-27 12:21:39 +0100

    Add renderShows modules

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,9 +1,13 @@
+import renderShows from "./renderShows.js";
+
 const baseurl = `https://api.tvmaze.com/shows`;
 
 const fetchSeries = async () => {
     const response = await fetch(baseurl)
     const data = await response.json()
     console.log(data)
+
+    renderShows(data)
 }
 
 export default fetchSeries
---
+++ src/modules/renderShows.js
@@ -0,0 +1,25 @@
+const mainSection = document.querySelector('.main')
+
+const renderShows = (shows) => {
+    shows.forEach((show) => {
+        const html = `
+            <div class="series-div" id="${show.id}" style="background: url(&quot;${show.image.original}&quot;) 0% 0% / cover; display: block;">
+            <i class="fa-solid fa-circle-info"></i>
+            <div class="about-series">
+                <h2 class="name">${show.name}</h2>
+                <div class="like-comment">
+                    <div class="like-div">
+                        <p class="likes">2 Likes</p>
+                        <p class="likes-icon">Like <i class="fa-solid fa-thumbs-up"></i></p>
+                    </div>
+                    <div class="comment-div">
+                        <p class="comment">Comment <i class="fa-solid fa-comment"></i></p>
+                    </div>
+                </div>
+            </div>
+        `;
+        mainSection.insertAdjacentHTML('beforeend', html)
+    })
+}
+
+export default renderShows
