40fd368edc322df9451ad168a038a97e0a36654f
Ogaga <iyaraogaga69@gmail.com>
2022-10-28 18:47:47 +0100

    Uncompleted function

---
+++ src/modules/searchFunction.js
@@ -0,0 +1,27 @@
+import { startsWith } from "lodash";
+import renderShows from "./renderShows";
+
+const search = document.querySelector(".search-input");
+const baseurl = `https://api.tvmaze.com/shows`;
+const searchKey = search.value
+
+const searchFunction = async () => {
+    const response = await fetch(baseurl)
+    let data = await response.json()
+
+    // data = data.filter((show) => {
+    //     return show.name.startsWith(searchKey)
+    // })
+
+    // console.log(data)
+    
+    
+
+    // console.log(searchKey)
+    // search.addEventListener('keydown', (e) => {
+    //     if (e.key === )
+    //     renderShows(data)
+    // })
+} 
+
+export default searchFunction
