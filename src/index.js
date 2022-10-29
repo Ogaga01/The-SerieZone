287b5d35334bdc219c4749004cb4668ccb20b797
Ogaga <iyaraogaga69@gmail.com>
2022-10-29 06:55:50 +0100

    Add search function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import fetchSeries from './modules/API';
 import mobileNav from './modules/mobileNav';
+import searchFunction from './modules/searchFunction';
 
 fetchSeries();
 mobileNav();
+searchFunction()
--- src/modules/searchFunction.js
+++ src/modules/searchFunction.js
@@ -1,25 +1,42 @@
-// import { startsWith } from 'lodash';
-// import renderShows from './renderShows';
+import { startsWith } from 'lodash';
+import renderShows from './renderShows';
 
-// const search = document.querySelector('.search-input');
-// const baseurl = 'https://api.tvmaze.com/shows';
-// const searchKey = search.value;
+const search = document.querySelector('.search-input');
+const baseurl = 'https://api.tvmaze.com/shows';
+const searchKey = search.value; 
 
-// const searchFunction = async () => {
-//   const response = await fetch(baseurl);
-//   const data = await response.json();
+const searchFunction = async () => {
+  const response = await fetch(baseurl);
+    const data = await response.json();
+    
 
-//   // data = data.filter((show) => {
-//   //     return show.name.startsWith(searchKey)
-//   // })
+    search.addEventListener('input', (e) => {
+        console.log(e.target.value)
+        const [first, ...rest] = e.target.value
+        const newFirst = first.toUpperCase()
+        const capitalised = newFirst + rest
+        console.log(rest)
+        console.log(capitalised)
+        const newData = data.filter((show) => {
+            return show.name.startsWith(capitalised)
+        })
+        renderShows(newData)
+    })
 
-//   // console.log(data)
 
-//   // console.log(searchKey)
-//   // search.addEventListener('keydown', (e) => {
-//   //     if (e.key === )
-//   //     renderShows(data)
-//   // })
-// };
 
-// export default searchFunction;
+
+  // data = data.filter((show) => {
+  //     return show.name.startsWith(searchKey)
+  // })
+
+  // console.log(data)
+
+  // console.log(searchKey)
+  // search.addEventListener('keydown', (e) => {
+  //     if (e.key === )
+  //     renderShows(data)
+  // })
+};
+
+export default searchFunction;
